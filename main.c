ca27ae52f2655756dbd627c2406afff4a74db31f
Marcelo Alves <161260493+marcelodev30@users.noreply.github.com>
2024-12-10 23:47:15 +0000

    ...

--- main.c
+++ main.c
@@ -2,19 +2,6 @@
 
 void main(){
     printf("Hello Horld\n");
-
-
-    struct Aluno
-    {
-        char name[50];
-        int matricula;
-        float nota;
-    };
-    
-
-    struct Aluno marcelo ={"Marcelo Alves",202312040020,10.0};
-    printf(marcelo.name);
-    
    
     
 }
